36fa6cf6e231a75527c9e6cbe205c17247bf2d62
Kurogami02 <89687091+Kurogami02@users.noreply.github.com>
2021-09-18 16:19:05 +0700

    add list

--- list.py
+++ list.py
@@ -1,10 +1,10 @@
 x= [2,6,3,8,1,4,7]
-a= len(x)
+len(x)
 
-for i in range(0,a-1):
-    for j in range(i+1,a):
+for i in range(0,len(x)):
+    for j in range(i+1,len(x)):
         if(x[i]> x[j]):
-            b=x[i]
+            t=x[i]
             x[i]=x[j]
-            x[j]=b
+            x[j]=t
 print(x)
